f335ea172f5f4173d3e44355c39751c3c57204a6
fer-okamura <https://github.com/fer-okamura>
2024-12-28 18:16:58 -0300

    Aula 1

---
+++ Lógica de Programação/Primeiros Passos/Tipos_de_Dados.js
@@ -0,0 +1,40 @@
+let idade = 30;
+
+let preco = 99.99;
+
+console.log("Número (inteiro):", idade);
+
+console.log("Número ( ponto flutuante):", preco);
+
+/*let nome = "Fernando";
+
+let saudacao ="Olá, Mundo!";
+
+console.log("String:", nome);
+
+console.log("String:", saudacao); */
+
+
+let estaLogado = true;
+
+let maiorDeIdade = false;
+
+console.log("Boolean (verdadeiro):", estaLogado);
+
+console.log("Boolean (falso):", maiorDeIdade);
+
+let endereco;
+
+console.log(" Undefined:", endereco);
+
+let salario = null;
+
+console.log("Null:", salario);
+
+let id = Symbol("id");
+
+console.log("Symbol:", id.toString() );
+
+let numeroGrande = 1233702477387017410041410410701714184170178;
+
+console.log ("BigInt:", numeroGrande);
