42ed96d390534840d091a531024a54b3432cf255
fer-okamura <https://github.com/fer-okamura>
2024-12-30 13:19:06 -0300

    criado var_let_const.js

---
+++ Lógica de Programação/Primeiros Passos/var_let_const.js
@@ -0,0 +1,23 @@
+var nome = "Fer";
+
+console.log (nome);
+
+var idade = 36;
+
+console.log(idade);
+
+var nome = "Maria";
+
+console.log(nome);
+
+const URL = "https://www.google.com.br";
+
+console.log(URL);
+
+let idade1 = 38;
+console.log("A idade é:",idade1);
+
+/* Existem 3 tipos de declarar as variáveis:  let , const  e  var.
+Entre as 3, var é mais antiga e usanda para funções e escopo global(pouco usado),
+e a let usado com mais frequencia. const é usado normalmente quando não quer alterar a variável)*/
+
